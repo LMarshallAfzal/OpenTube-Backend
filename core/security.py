ada82e1c4635026e5f5fc7e923f7493eb6be8a61
Leonard Marshall Afzal <86576612+LMarshallAfzal@users.noreply.github.com>
2025-08-03 01:54:19 +0100

    Implement JWT token auth verification
    - Add OAuth bearer to for token extraction from authorization header
    - Create get_current_user function to decode and verify JWT, and validate subject claim

--- core/security.py
+++ core/security.py
@@ -1,6 +1,11 @@
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 from core.config import settings
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/auth/login")
 
 
 def create_access_token(data: dict):
@@ -10,3 +15,22 @@ def create_access_token(data: dict):
         settings.SECRET_KEY,
         algorithm=settings.ALGORITHM
     )
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY,
+                             algorithms=[settings.ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+
+    # TODO: Replace with actual user from DB
+    return username
