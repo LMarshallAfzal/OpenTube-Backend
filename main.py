051f7c4c70246d0dc2db31c58ee146bc0f7b9cef
Leonard Marshall Afzal <86576612+LMarshallAfzal@users.noreply.github.com>
2025-08-10 14:03:27 +0100

    Add CORS middleware

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlmodel import SQLModel
 
 from routes import auth, video
@@ -9,6 +10,19 @@ from middleware.request_logger import log_request_body
 app = FastAPI()
 
 app.middleware("http")(log_request_body)
+
+origins = [
+    "http://localhost:8000",
+]
+
+# Change these in production
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router, prefix="/api/auth")
 app.include_router(video.router, prefix="/api/videos", tags=["videos"])
