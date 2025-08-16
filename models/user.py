218d0806ccd83aa46250f8d46446058bfe481ce1
Leonard Marshall Afzal <86576612+LMarshallAfzal@users.noreply.github.com>
2025-08-16 18:09:41 +0100

    Refactor models/ package
    - Update user model with explict type hints and unique flag

--- models/user.py
+++ models/user.py
@@ -1,9 +1,12 @@
+from __future__ import annotations
+
+from typing import Optional
 from sqlmodel import SQLModel, Field
 
 
 class User(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    username: str = Field(unique=True)
+    id: Optional[int] = Field(default=None, primary_key=True)
+    username: str = Field(sa_column_kwargs={"unique": True})
     hashed_password: str
 
     # history: List["History"] = Relationship(back_populates="user")
