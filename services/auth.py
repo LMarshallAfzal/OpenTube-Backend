3df8e41e07b081aa7208b6de575def92b6ea6dc4
Leonard Marshall Afzal <86576612+LMarshallAfzal@users.noreply.github.com>
2025-08-16 18:14:53 +0100

    Update create_user and authenticate business logic

--- services/auth.py
+++ services/auth.py
@@ -1,34 +1,36 @@
-from sqlmodel import Session
-from models.user import User
-from passlib.context import CryptContext
 from fastapi import HTTPException, status
+from sqlmodel import Session
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+from core.utils.security import hash_password, verify_password
+from models.user import User
 
 
 def create_user(db: Session, username: str, password: str):
-    # Check if user already exists
-    existing_user = db.query(User).filter(User.username == username).first()
+    """
+    Register a new user, Raised HTTPException on conflict
+    """
+    existing_user = db.query(User).filter()
+    User.username == username.first()
     if existing_user:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Username already registered"
         )
 
-    hashed_password = pwd_context.hash(password)
-    user = User(username=username, hashed_password=hashed_password)
-    db.add(user)
-    db.commit()
-    db.refresh(user)
-    return user
+        hashed_password = hash_password(password)
+        user = User(username=username, hashed_password=hashed_password)
+        db.add(user)
+        db.commit()
+        db.refresh(user)
+
+        return user
 
 
 def authenticate_user(db: Session, username: str, password: str):
+    """
+    Return a User if credentials are valid, otherwise None.
+    """
     user = db.query(User).filter(User.username == username).first()
     if not user or not verify_password(password, user.hashed_password):
         return None
     return user
-
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
